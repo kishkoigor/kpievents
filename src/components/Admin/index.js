23067407e843eb666f97ca644f2ac9eacb0bd964
_catskin <D7wX8U4ZBEgUsNz4>
2019-12-11 03:54:42 +0200

    datetimepicker install

--- src/components/Admin/index.js
+++ src/components/Admin/index.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
+import DateTimePicker from "react-datetime-picker/src/DateTimePicker";
 
 export default class Admin extends Component {
+
+  state = {
+    date: new Date(),
+  }
+
+  onChange = date => this.setState({ date })
 
   render() {
     return (
       <div>
         Admin
+        <DateTimePicker
+          onChange={this.onChange}
+          value={this.state.date}
+        />
       </div>
     );
   }
